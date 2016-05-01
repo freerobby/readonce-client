799497160b7b5d0dac538b1d09084ab709d5bd79
Robby Grossman <robby@freerobby.com>
2016-05-01 01:06:57 -0400

    Add status check and blocking functionality.

--- lib/readonce.rb
+++ lib/readonce.rb
@@ -4,14 +4,33 @@ require 'httparty'
 
 class ReadOnce
   BASE_URI = 'http://readonce-production.herokuapp.com'
-  def initialize(data)
-    puts data
+
+  def self.from_key(key)
+    r = ReadOnce.new
+    r.instance_variable_set(:@key, key)
+    r
+  end
+
+  def self.from_data(data)
     response = HTTParty.post "#{BASE_URI}/create", body: data
-    self.key = response.body
+    r = ReadOnce.new
+    r.instance_variable_set(:@key, response.body)
+    r
   end
 
   def read_url
     "#{BASE_URI}/#{key}"
+  end
+
+  def exists?
+    response = HTTParty.get "#{BASE_URI}/status/#{key}"
+    response.code == 200 ? true : false
+  end
+
+  def block_while_exists
+    while exists? do
+      sleep 1
+    end
   end
 
   private
--- spec/readonce_spec.rb
+++ spec/readonce_spec.rb
@@ -7,8 +7,23 @@ describe Readonce do
   describe '#initialize' do
     it 'creates a readonce file' do
       VCR.use_cassette 'create-file' do
-        f = ReadOnce.new('dummydata')
+        f = ReadOnce.from_data('dummydata')
         expect(f.read_url).to eql('http://readonce-production.herokuapp.com/3a84c711bcb5ff2afaa23e212f60bb04')
+      end
+    end
+  end
+
+  describe '#exists?' do
+    it 'returns true when exists' do
+      VCR.use_cassette 'key-exists' do
+        f = ReadOnce.from_key('63d2e85319449527d87468a3833365c4')
+        expect(f.exists?).to be_truthy
+      end
+    end
+    it 'returns true when exists' do
+      VCR.use_cassette 'key-does-not-exist' do
+        f = ReadOnce.from_key('a2b7385319449527d87468a3833365c4')
+        expect(f.exists?).to be_falsey
       end
     end
   end
