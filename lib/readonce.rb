7c3c7614bc910ea3b8f319206865e60f3d63c59a
Robby Grossman <robby@freerobby.com>
2016-05-05 00:42:23 -0400

    Add support for version requirements.

--- lib/readonce.rb
+++ lib/readonce.rb
@@ -1,4 +1,4 @@
-require 'readonce/version'
+require_relative 'readonce/version'
 
 require 'httparty'
 
@@ -8,6 +8,18 @@ class ReadOnce
   USER_AGENT_HEADER = {'User-Agent' => "ReadOnce Gem/#{Readonce::VERSION}"}
   base_uri BASE_URI
 
+  def self.exit_if_gem_outdated!
+    response = get '/minimum-ruby-client-version', headers: USER_AGENT_HEADER
+    major, minor, patch = response.body.split('.').map{|m| m.to_i}
+
+    if Readonce::MAJOR_VERSION < major || Readonce::MINOR_VERSION < minor || Readonce::PATCH_VERSION < patch
+      required_version = "#{major}.#{minor}.#{patch}"
+      puts "ERROR: Gem version is #{Readonce::VERSION}, but #{required_version} or later is now required."
+      puts 'Please run: gem install readonce'
+      exit
+    end
+  end
+
   def self.from_key(key)
     r = ReadOnce.new
     r.instance_variable_set(:@key, key)
@@ -15,6 +27,7 @@ class ReadOnce
   end
 
   def self.from_data(data)
+    self.exit_if_gem_outdated!
     response = post '/create', body: data, headers: {'Content-type' => 'text/plain'}.merge(USER_AGENT_HEADER)
     r = ReadOnce.new
     r.instance_variable_set(:@key, response.body)
@@ -26,16 +39,19 @@ class ReadOnce
   end
 
   def read?
+    self.exit_if_gem_outdated!
     response = self.class.get "/status/#{key}", headers: USER_AGENT_HEADER
     !JSON.parse(response.body)['accessed_at'].nil?
   end
 
   def exists?
+    self.exit_if_gem_outdated!
     response = self.class.get "/status/#{key}", headers: USER_AGENT_HEADER
     response.code == 200
   end
 
   def status
+    self.exit_if_gem_outdated!
     response = self.class.get "/status/#{key}", headers: USER_AGENT_HEADER
     JSON.parse(response.body)
   end
